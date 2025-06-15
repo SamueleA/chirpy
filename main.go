5de084672bcff23b78edf94738e61cb0cdc7ceda
samuelea <batonenbois@gmail.com>
2025-06-15 08:39:53 -0400

    added custom handler

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 func main() {
 	serveMux := http.NewServeMux()
 
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/assets/", http.StripPrefix("/app/assets", http.FileServer(http.Dir("assets"))))
+	serveMux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 	
 	server := &http.Server{
 		Addr: ":8080",
